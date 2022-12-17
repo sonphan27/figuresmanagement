1b66a62123baaad29af216c0cfe2143d2dd4a1a4
sonph <sonphan270497@gmail.com>
2022-12-17 20:11:29 +0700

    add data + more web

---
+++ db/create_table_products.sql
@@ -0,0 +1,28 @@
+CREATE TYPE public.e_shipping_status AS ENUM (
+    'wished',
+    'pre-ordered',
+    'ordered',
+    'shipping',
+    'shipped',
+    'sold'
+);
+CREATE TYPE public.e_status AS ENUM (
+    'new',
+    '2nd',
+    '2nd (no box)'
+);
+
+create table if not exists public.products (
+  id serial primary key,
+  code text,
+  name text,
+  brand_id int,
+  company_id int,
+  bought_price float,
+  sold_price float,
+  serie_id int,
+  status e_status default 'new',
+  shipping_status e_shipping_status default 'shipped',
+  bought_from text,
+  active boolean default true
+)
--- db/create_table_users.sql
+++ db/create_table_users.sql
@@ -1,5 +1,6 @@
 create table if not exists public.users (
   id serial primary key,
   name text not null,
+  pass text not null,
   active boolean default true
 )
