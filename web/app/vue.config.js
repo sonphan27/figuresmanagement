c29f2d17d42373de6ee6474c0aeb4f317a40b04e
sonph <sonphan270497@gmail.com>
2022-12-29 21:15:10 +0700

    add config to connect api

--- web/app/vue.config.js
+++ web/app/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/(api|docs|openapi.json|redoc)': {
+        target: 'http://api:8000',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  }
 })
